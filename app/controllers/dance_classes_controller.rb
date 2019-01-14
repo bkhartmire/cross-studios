eb65bd304a58e5161e26f7f913cc8cb968ed5eb7
bkhartmire <bkhartmire@gmail.com>
2019-01-13 17:47:39 -0800

    trying to reference scraper in controller and render results in view

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,9 +1,7 @@
 class DanceClassesController < ApplicationController
   def new
-    dance_class = Class.new
-    url = 'https://www.playgroundla.dance/schedule/'
-    doc = Nokogiri::HTML(open(url))
-    @data = doc.css('span.hc_date').text 
+    DanceClassScraper.new.make_dance_classes
+    @classes = DanceClass.all
   end
 
 end
