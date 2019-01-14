e2d0e7e5cac041182c638f699b96615420402649
bkhartmire <bkhartmire@gmail.com>
2019-01-13 18:50:37 -0800

    do i need to require relative for scraper and dance class model?

--- app/models/dance_class_scraper.rb
+++ app/models/dance_class_scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require_relative './dance_class.rb'
 
 class DanceClassScraper
   #for now only using playground webpage
@@ -9,20 +10,20 @@ class DanceClassScraper
 
 #date is span.hc_date
   def get_schedule
-    self.get__page.css("tr.schedule_header")
+    self.get__page.css(".schedule_header")
   end
 
   def make_dance_classes
     self.get_schedule.each do |day_row|
-      date = day_row.css("span.hc_date").text
+      date = day_row.css(".hc_date").text
       row.css("tr").each do |class_row|
         dance_class = DanceClass.new
         dance_class.date = date
         console.log('inside method')
         #there are two inner spans for start time and ending time. Do I need to break this up?
-        dance_class.time = class_row.css("span.hc_time").text
-        dance_class.name = class_row.css("span.classname").text
-        instructor_name = class_row.css("span.trainer").text
+        dance_class.time = class_row.css(".hc_time").text
+        dance_class.name = class_row.css(".classname").text
+        instructor_name = class_row.css(".trainer").text
         class_instructor = Instructor.find_or_create_by(name: instructor_name)
         dance_class.instructor_id = class_instructor.id
         dance_class.save
