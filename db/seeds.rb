62a76d616b3d3c9b7323fbed37cd41d21743b950
bkhartmire <bkhartmire@gmail.com>
2019-01-13 16:17:34 -0800

    studio seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+studios = Studio.create([
+    {name: 'Millennium Dance Complex', website: 'http://millenniumdancecomplex.com'},
+    {name: 'Movement Lifestyle', website: 'https://www.themovementlifestyle.com'},
+    {name: 'Playground', website: 'https://www.playgroundla.dance'}
+])
