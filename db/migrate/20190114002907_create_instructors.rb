64b9f4445b93b664508f121fba114c763ef8a299
bkhartmire <bkhartmire@gmail.com>
2019-01-16 15:58:04 -0800

    manually create playground instructor seeds and give default favorited number of 0

--- db/migrate/20190114002907_create_instructors.rb
+++ db/migrate/20190114002907_create_instructors.rb
@@ -2,7 +2,7 @@ class CreateInstructors < ActiveRecord::Migration[5.2]
   def change
     create_table :instructors do |t|
       t.string :name
-      t.integer :favorited_count
+      t.integer :favorited_count, default: 0
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,34 @@ addresses = Address.create([
   {line_1: '11105 Weddington St,', line_2: 'North Hollywood, CA 91601', studio_id: Studio.find_by(name: 'Movement Lifestyle').id},
   {line_1: '7375 Melrose Ave,', line_2: 'Los Angeles, CA 90046', studio_id: Studio.find_by(name: 'The Playground LA').id}
 ])
+
+instructors = Instructor.find_or_create_by([
+  {name: 'Margie Dilivio'},
+  {name: 'Nick Gilligan'},
+  {name: 'Charlie Bartley'},
+  {name: 'Kennis Marquis'},
+  {name: 'Nicole Kirkland'},
+  {name: 'Sam Allen'},
+  {name: 'Ava Bernstine-Mitchell'},
+  {name: 'Dexter Carr'},
+  {name: 'Michelle Janine'},
+  {name: 'Marissa Heart'},
+  {name: 'Julian Deguzman'},
+  {name: 'Phil Wright'},
+  {name: 'Rex Kline'},
+  {name: 'Shane Bruce'},
+  {name: 'Hamilton Evans'},
+  {name: 'Kenny Wormald'},
+  {name: 'Alexis Beauregard'},
+  {name: 'Brinn Nicole'},
+  {name: 'Cisco Ruelas'},
+  {name: 'JJ Dancer'},
+  {name: 'Charlie Bartley'},
+  {name: 'Guy Groove'},
+  {name: 'Rumer Noel'},
+  {name: 'Margie Dilivio'}
+])
+
+dance_classes = DanceClass.create([
+  {name: , studio_id: , instructor_id:, day: ,time: },
+])
