8ac696fe84f1b17a57ea7d0ed29de8e143d50461
bkhartmire <bkhartmire@gmail.com>
2019-01-17 13:25:40 -0800

    set up danceclass component

---
+++ client/src/components/DanceClass.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Instructor from './Instructor'
+
+const DanceClass=({danceClass}) =>
+  <div>
+    <h1>This is the DanceClass component.</h1>
+  </div>
+
+export default DanceClass
