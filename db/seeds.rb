3ceb1db4490635a1d964ad15221f18ecf3b3649a
bkhartmire <bkhartmire@gmail.com>
2019-01-16 16:21:03 -0800

    more playground seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -59,4 +59,10 @@ dance_classes = DanceClass.create([
   {name: 'Beginner Heels', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Alexis Beauregard').id, day: 'FRIDAY',time: '5:00pm-6:00pm'},
   {name: 'Heels', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Brinn Nicole').id, day: 'FRIDAY',time: '6:00pm-8:00pm'},
   {name: 'Heels', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Cisco Ruelas').id, day: 'FRIDAY',time: '8:30pm-10:00pm'},
+  {name: 'Bangin Body', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'JJ Dancer').id, day: 'SATURDAY',time: '10:30am-11:30am'},
+  {name: 'Hip Hop', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Charlie Bartley').id, day: 'SATURDAY', time: '12:00pm-1:15pm'},
+  {name: 'Beginner Heels', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Alexis Beauregard').id, day: 'SATURDAY',time: '1:30pm-2:45pm'},
+  {name: 'Hip Hop', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Dexter Carr').id, day: 'SATURDAY',time: '3:00pm-4:30pm'},
+  {name: 'Hip Hop', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Guy Groove').id, day: 'SUNDAY',time: '1200pm-1:00pm'},
+  {name: 'Jazz Funk', studio_id: Studio.find_by(name: 'The Playground LA').id, instructor_id: Instructor.find_by(name: 'Rumer Noel').id, day: 'SUNDAY',time: '1:00pm-2:15pm'},
 ])
