e0958422bae4b24295e797bc28c0987d214ba330
bkhartmire <bkhartmire@gmail.com>
2019-01-17 23:31:28 -0800

    edit rendered dance_class json object

--- client/src/components/DanceClass.js
+++ client/src/components/DanceClass.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Instructor from './Instructor'
 
 const DanceClass=({danceClass}) =>
-  <div class="dance_class_listing">
-    <h4>{danceClass.instructor}: {danceClass.name}</h4>
+  <div className="dance_class_listing">
+    <h4>{danceClass.instructor_name}: {danceClass.name}</h4>
     <h2></h2>
   </div>
 
