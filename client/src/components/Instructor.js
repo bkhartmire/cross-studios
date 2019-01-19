cbfec5af09a8d0ce3428158fc5ae5e839f45725c
bkhartmire <bkhartmire@gmail.com>
2019-01-18 23:29:55 -0800

    delete unused code and update video_url

--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -4,35 +4,33 @@ class Instructor extends Component {
   constructor(props){
     super(props)
     this.state= {
-      name: ''
+      name: '',
+      video_url: ''
     }
   }
 
   updateData() {
     this.setState({
-      name: this.name
+      name: this.name,
+      video_url: this.video_url
     })
-    debugger
   }
 
   componentDidMount() {
     this.id = this.props.match.params.id
-    //this.setState({
-    //  name: {this.props.location.state.instructor.name}
-    //})
     this.name = this.props.location.state.instructor.name
-    //const {instructor} = this.props.location.state
+    this.video_url = this.props.location.state.instructor.video_url
     this.updateData()
-    debugger
   }
 
 
   render(){
-    console.log(this.id)
+    const instructor = this.state
     return(
       <div>
         <h1>This is the instructor component.</h1>
-        <h2>{this.state.name}</h2>
+        <h2>{instructor.name}</h2>
+        <iframe width="560" height="315" src={instructor.video_url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
       </div>
     )
