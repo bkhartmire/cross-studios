e59785e30465fe7173631531844320124ac52c7b
bkhartmire <bkhartmire@gmail.com>
2019-01-19 00:17:23 -0800

    nest instructor dance_classes and studio address in danceclass json object

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -4,9 +4,9 @@ class DanceClassesController < ApplicationController
     if DanceClass.all.none?{|dance_class| dance_class.studio_id == Studio.find_by(name: 'Millennium Dance Complex').id}
       DanceClassScraper.new.make_dance_classes
     end
+    @classes = DanceClass.all
     #include associated instructor and studio in json object
-    @classes = DanceClass.all.as_json(methods:[:instructor, :studio])
-    render json: @classes
+    render json: @classes, include: {instructor: {include: :dance_classes}, studio: {include: :address}}
   end
 
 end
--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,4 +1,5 @@
 class InstructorsController < ApplicationController
+  #you can delete this. we don't fetch this. instead nesting associated instructors json data in dance_class json objects
   def index
     @instructors = Instructor.all.as_json(methods:[:dance_classes])
     render json: @instructors
