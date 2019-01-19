d7c69b569c61ab332fdfc31e263553173422d181
bkhartmire <bkhartmire@gmail.com>
2019-01-18 23:23:01 -0800

    able to change instructor name state

--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
 
 class Instructor extends Component {
-  componentDidMount() {
-    const {id} = this.props.match.params
-    const {instructor} = this.props.location.state
+  constructor(props){
+    super(props)
+    this.state= {
+      name: ''
+    }
+  }
+
+  updateData() {
+    this.setState({
+      name: this.name
+    })
     debugger
   }
+
+  componentDidMount() {
+    this.id = this.props.match.params.id
+    //this.setState({
+    //  name: {this.props.location.state.instructor.name}
+    //})
+    this.name = this.props.location.state.instructor.name
+    //const {instructor} = this.props.location.state
+    this.updateData()
+    debugger
+  }
+
+
   render(){
+    console.log(this.id)
     return(
       <div>
         <h1>This is the instructor component.</h1>
+        <h2>{this.state.name}</h2>
+
       </div>
     )
   }
