6bc3395843c57bb41eed9658fba5309099d25655
bkhartmire <bkhartmire@gmail.com>
2019-01-18 22:49:16 -0800

    update instructor react component to have state

--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React {Component}, from 'react'
 
-const Instructor = ({instructor}) =>
-  <div>
-    <h1>This is the instructor component.</h1>
-  </div>
+class Instructor extends React {
+  componentDidMount() {
+
+  }
+  render(){
+    return(
+      <div>
+        <h1>This is the instructor component.</h1>
+      </div>
+    )
+  }
+}
+
 export default Instructor
