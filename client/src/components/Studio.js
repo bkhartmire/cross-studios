6bbab29b8dfc50688cc0d0918302fa4bda53d760
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:09:43 -0800

    create Studio component and map studios array in home container

---
+++ client/src/components/Studio.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class Studio extends Component {
+
+}
+
+export default Studio
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 //create this action!
 import { fetchStudios } from '../actions/studioActions'
+import Studio from '../components/Studio'
 
 class Home extends Component {
   componentDidMount(){
@@ -8,9 +9,13 @@ class Home extends Component {
   }
 
   render(){
+    const {studios} = this.props
     return(
       <div>
         <h1>Manage your class schedule from these top LA dance studios!</h1>
+        <ul>
+          {studios.map((studio) => <Studio key={studio.id} studio={studio}/>)}
+        </ul>
       </div>
     )
   }
