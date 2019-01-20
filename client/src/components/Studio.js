aff7897848e9940d5d65c157af515bfd381044d7
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:17:15 -0800

    code out studio component

--- client/src/components/Studio.js
+++ client/src/components/Studio.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
 
 class Studio extends Component {
-
+  render() {
+    const {studio} = this.props
+    return(
+      <div className="studio_listing">
+        <h3>{studio.name}</h3>
+      </div>
+    )
+  }
 }
 
 export default Studio
