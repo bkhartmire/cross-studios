7c4667dd2fea4e2b286d9e1270ef2b2b79e4c467
bkhartmire <bkhartmire@gmail.com>
2019-01-19 20:19:27 -0800

    add signup component code example

--- client/src/containers/Signup.js
+++ client/src/containers/Signup.js
@@ -3,15 +3,61 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
 
-import { loginUser } from '../actions/userActions'
+import { signupUser } from '../actions/userActions'
 
-class Login extneds Component {
-  constructor(){
+class Signup extends Component {
+  constructor() {
     super()
-    //Do you have to get rid of this filler code??
+
     this.state = {
-      username: 'demo',
-      password: 'password'
+      username: '',
+      password: '',
+      password_confirmation: '',
+      firstname: '',
+      lastname: ''
     }
+
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    const field = e.target.name
+    let state = this.state
+
+    state[field] = e.target.value
+    this.setState(state)
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+
+    const user = this.state
+    this.props.signupUser(user, () => this.props.history.push('/'))
+  }
+
+  render() {
+    const { username, password, password_confirmation, firstname, lastname } = this.state
+
+    return (
+      <div>
+        <h1>Sign Up</h1>
+        <form onSubmit={ this.onSubmit }>
+          <input name="username" placeholder="Username" value={ username } onChange={ this.onChange }/><br/>
+          <input type='password' name="password" placeholder="Password" value={ password } onChange={ this.onChange }/><br/>
+          <input type='password' name="password_confirmation" placeholder="Password Confirmation" value={ password_confirmation } onChange={ this.onChange }/><br/>
+          <input name="firstname" placeholder="Firstname" value={ firstname } onChange={ this.onChange }/><br/>
+          <input name="lastname" placeholder="Lastname" value={ lastname } onChange={ this.onChange }/><br/>
+          <button type="submit">Signup</button>
+        </form>
+        <Link to='/login'>Log In</Link>
+      </div>
+    )
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  signupUser
+}, dispatch)
+
+export default withRouter(connect(null, mapDispatchToProps)(Signup))
