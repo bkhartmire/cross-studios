6432fdfd8b9445533090ac574252803ffc502ddf
bkhartmire <bkhartmire@gmail.com>
2019-01-19 20:21:04 -0800

    change stylistic preference on => handleChange

--- client/src/containers/Signup.js
+++ client/src/containers/Signup.js
@@ -17,11 +17,11 @@ class Signup extends Component {
       lastname: ''
     }
 
-    this.onSubmit = this.onSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  onChange(e) {
+  handleChange(e) {
     const field = e.target.name
     let state = this.state
 
@@ -29,7 +29,7 @@ class Signup extends Component {
     this.setState(state)
   }
 
-  onSubmit(e) {
+  handleSubmit(e) {
     e.preventDefault()
 
     const user = this.state
@@ -42,12 +42,12 @@ class Signup extends Component {
     return (
       <div>
         <h1>Sign Up</h1>
-        <form onSubmit={ this.onSubmit }>
-          <input name="username" placeholder="Username" value={ username } onChange={ this.onChange }/><br/>
-          <input type='password' name="password" placeholder="Password" value={ password } onChange={ this.onChange }/><br/>
-          <input type='password' name="password_confirmation" placeholder="Password Confirmation" value={ password_confirmation } onChange={ this.onChange }/><br/>
-          <input name="firstname" placeholder="Firstname" value={ firstname } onChange={ this.onChange }/><br/>
-          <input name="lastname" placeholder="Lastname" value={ lastname } onChange={ this.onChange }/><br/>
+        <form onSubmit={ this.handleSubmit }>
+          <input name="username" placeholder="Username" value={ username } onChange={ this.handleChange }/><br/>
+          <input type='password' name="password" placeholder="Password" value={ password } onChange={ this.handleChange }/><br/>
+          <input type='password' name="password_confirmation" placeholder="Password Confirmation" value={ password_confirmation } onChange={ this.handleChange }/><br/>
+          <input name="firstname" placeholder="Firstname" value={ firstname } onChange={ this.handleChange }/><br/>
+          <input name="lastname" placeholder="Lastname" value={ lastname } onChange={ this.handleChange }/><br/>
           <button type="submit">Signup</button>
         </form>
         <Link to='/login'>Log In</Link>
