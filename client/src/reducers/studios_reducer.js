ab57cec23e56949c014858b65d9a25c378f7e44c
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:15:39 -0800

    build out studios reducer

--- client/src/reducers/studios_reducer.js
+++ client/src/reducers/studios_reducer.js
@@ -1,3 +1,15 @@
 export default function studiosReducer(
-  
-)
+  state = {
+    loadding: false,
+    all: []
+  }, action) {
+    switch (action.type) {
+      case 'LOADING_STUDIOS':
+        return { ...state, loading: true}
+      case 'FETCH_STUDIOS':
+        return {...state, loading: false, all: action.payload}
+        console.log(state)
+      default:
+        return state
+    }
+  }
