f6eb449fb3d35ad2ee203294d245dcfcb707d90c
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:52:17 -0800

    render studio addresses

--- client/src/components/Studio.js
+++ client/src/components/Studio.js
@@ -6,6 +6,9 @@ class Studio extends Component {
     return(
       <div className="studio_listing">
         <h3>{studio.name}</h3>
+        <h5>{studio.address.line_1}</h5>
+        <h5>{studio.address.line_2}</h5>
+        <br></br>
       </div>
     )
   }
