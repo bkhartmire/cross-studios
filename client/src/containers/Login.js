8d852f87e9e3a6c383a4d3371d3b06bbb7e1a8cd
bkhartmire <bkhartmire@gmail.com>
2019-01-19 20:18:50 -0800

    Login Component

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -13,5 +13,42 @@ class Login extneds Component {
       username: 'demo',
       password: 'password'
     }
+    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+  }
+
+  handleChange(e) {
+    const field = e.target.name
+    let state = this.state
+    state[field] = e.target.value
+    this.setState(state)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    const user = this.state
+    this.props.loginUser(user, () => this.props.history.push('/'))
+  }
+
+  render() {
+    const { username, password } = this.state
+
+    return(
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={ this.handleSubmit }>
+            <input name="username" placeholder="Username" value={ username } onChange={ this.handleChange }/><br/>
+            <input type='password' name="password" placeholder="Password" value={ password } onChange={ this.handleChange }/><br/>
+            <button type="submit">Login</button>
+          </form>
+          <Link to='/signup'>Sign Up</Link>
+      </div>
+    )
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  loginUser
+}, dispatch)
+
+export default withRouter(connect(null, mapDispatchToProps)(Login))
