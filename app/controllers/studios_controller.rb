07c806555ad54a8d0cb9588ca8e5297576831ebc
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:05:39 -0800

    fix studios#index

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,7 +1,8 @@
-class InstructorsController < ApplicationController
+class StudiosController < ApplicationController
 
   def index
     @studios = Studio.all.as_json(methods:[:address])
+    render json: @studios
   end
 
 end
