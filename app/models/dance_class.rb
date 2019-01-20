1a51a13038e1099c79add047939b56da9d029750
bkhartmire <bkhartmire@gmail.com>
2019-01-19 18:11:37 -0800

    sketch out plan to sort danceclasses chronologically class method

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -2,4 +2,18 @@ class DanceClass < ApplicationRecord
   belongs_to :studio
   belongs_to :instructor
 
+  #add instance method to make day capital letter, then all lowercase
+
+
+  def self.sort_chronologically
+    self.sort_by_day.sort_by_time
+  end
+
+  #sort by day
+  def self.sort_by_day
+  end
+  #sort each group of days
+  def self.sort_by_time
+  end
+
 end
