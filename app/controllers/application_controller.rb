c22688d1d93805ad0797770f7187cecc31bcd9e3
bkhartmire <bkhartmire@gmail.com>
2019-01-20 13:28:26 -0800

    #get_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  def get_current_user
+    jwt_token = request.headers['HTTP_AUTHORIZATION'] # .gsub('Bearer ', '')
+
+    if jwt_token
+      user_info = Auth.decode(jwt_token)
+      user ||= User.find(user_info['user_id'])
+    end
+
+    user
+  end
 end
