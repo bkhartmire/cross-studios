aeff3ad46954d4aa5961ae2fc7e9e9fca24820c3
bkhartmire <bkhartmire@gmail.com>
2019-01-20 10:43:05 -0800

    create users model and est hasmany/hasmany w danceclasses

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_many :dance_classes
+end
