8c5cc256637aa00163c02a666047674ade037926
bkhartmire <bkhartmire@gmail.com>
2019-01-20 11:01:07 -0800

    build fetchstudios action modeled after fetch danceclasses

---
+++ client/src/actions/studioActions.js
@@ -0,0 +1,10 @@
+export const fetchStudios = () => {
+  return (dispatch) => {
+
+    dispatch({type: 'LOADING_STUDIOS'})
+    return fetch('/api/studios', {
+      accept: 'application/json',
+    }).then(resp => resp.clone().json())
+      .then(studios => dispatch({type: 'FETCH_STUDIOS', payload: studios}))
+  }
+}
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+//create this action!
 import { fetchStudios } from '../actions/studioActions'
 
 class Home extends Component {
   componentDidMount(){
     this.props.fetchStudios()
   }
+
+  render(){
+    return(
+      <div>
+        <h1>Manage your class schedule from these top LA dance studios!</h1>
+      </div>
+    )
+  }
 }
-const Home = () => {
-  return(
-    <div>
-      <h1>Manage your class schedule from these top LA dance studios!</h1>
-    </div>
-  )
-}
+
 
 export default Home
