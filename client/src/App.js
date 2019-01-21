7a4ed26b961f0929a861d831ee51e7f7edba3e68
bkhartmire <bkhartmire@gmail.com>
2019-01-20 20:01:39 -0800

    change fragment to div for styling purposes

--- client/src/App.js
+++ client/src/App.js
@@ -25,14 +25,14 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <React.Fragment>
+        <div className="App">
           <Route exact path='/' render={ () =>  <Home/>}/>
           <Route path='/dance_classes' component={DanceClassList} />
           <Route path='/instructors/:id' component={Instructor}/>
           <Route path='/signup' component={ () => <Signup handleSignupSubmit={this.handleSignupSubmit}/> }/>
           <Route path='/login' component={ () => (this.state.auth) ? <Redirect to="/"/> : <Login/> }/>
 
-        </React.Fragment>
+        </div>
       </BrowserRouter>
     );
   }
