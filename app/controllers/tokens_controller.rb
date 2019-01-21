3efd0b40a646205e6778792a3718bfc107fb6036
bkhartmire <bkhartmire@gmail.com>
2019-01-20 16:46:08 -0800

    tokens controller

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,2 +1,23 @@
 class TokensController < ApplicationController
+
+  def create
+    user = User.find_by(username: params[:username])
+    byebug
+    if user&.authenticate(params[:password])
+      render json: {
+        jwt: encode_token({id: user.id, username: user.username})
+      }
+    else
+      head :not_found
+    end
+  end
+
+  private
+  def encode_token(payload={})
+    exp = 24.hours.from_now
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+
 end
