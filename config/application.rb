7fdafb2b50f5472119b418a19b025377cfe1e6c9
bkhartmire <bkhartmire@gmail.com>
2019-01-20 17:58:48 -0800

    comment out application.rb code

--- config/application.rb
+++ config/application.rb
@@ -15,15 +15,17 @@ module ReactReduxGetTogetherApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.generators.system_tests = nil
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [
-          :delete, :put, :patch, :get, :post, :options
-        ]
-      end
-    end
+    #you added this code in first token auth youtube tutorial attempt:
+    # config.generators.system_tests = nil
+    #
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #     resource '*', headers: :any, methods: [
+    #       :delete, :put, :patch, :get, :post, :options
+    #     ]
+    #   end
+    # end
   end
 end
