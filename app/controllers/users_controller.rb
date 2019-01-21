bbbf6c6fe9c2f6ddcc8fd20f0a384c4b7ef372d6
bkhartmire <bkhartmire@gmail.com>
2019-01-20 18:26:06 -0800

    user#profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,23 @@
 class UsersController < ApiController
    before_action :require_login, except: [:create]
 
+   def create
+     #.create! will return user itself instead of boolean value
+     user = User.create!(user_params)
+     render json: {token: user.auth_token}
+   end
+
+   def profile
+     user = User.find_by_auth_token!(request_headers[:token])
+     render json: { user: {
+       username: user.username,
+       firstname: user.firstname,
+       lastname: user.lastname
+       }}
+   end
+
+
+
 
 
   # def create
