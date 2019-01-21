93ce4e8f409e1b8e4dbbc98778e80ebe1b8b2448
bkhartmire <bkhartmire@gmail.com>
2019-01-20 18:02:31 -0800

    apicontroller methods

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,16 @@
-class ApiController < ApplicationController
+ class ApiController < ApplicationController
+   def require_login
+     authenticate_token || render_unauthorized("Access denied")
+   end
+
+   def current_user
+     @current_user ||= authenticate_token
+   end
+
+   protected
+
+   def render_unauthorized(message)
+     errors = { errors: [detail: message]}
+     render json: errors, status: :unauthorized
+   end
 end
