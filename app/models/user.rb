ebbcf390c669323e67562d858aa67e9b8e1f64f5
bkhartmire <bkhartmire@gmail.com>
2019-01-20 18:15:35 -0800

    cleanup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
-  
+  has_many :dance_classes
+
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 
-  has_many :dance_classes
+  has_secure_password
+  has_secure_token :auth_token
+
+
 end
