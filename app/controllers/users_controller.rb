512b30352504a3371a155214ac5210bcc151fda5
bkhartmire <bkhartmire@gmail.com>
2019-01-21 10:12:33 -0800

    just had to clear session token and log in again...working now! add dance_classes to user object array

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,46 +13,12 @@ class UsersController < ApiController
      render json: { user: {
        username: user.username,
        firstname: user.firstname,
-       lastname: user.lastname
+       lastname: user.lastname,
+       dance_classes: user.dance_classes
       }}
    end
 
 
-
-
-
-  # def create
-  #   #user = User.create(user_params)
-  #
-  #   #if user
-  #   #  jwt = Auth.encrypt({ user_id: user.id })
-  #   #  render json: { jwt: jwt, current: user }
-  #   #else
-  #   #  render json: { error: 'Failed to Sign Up' }, status: 400
-  #   #end
-  #   input = User.new(user_params)
-  #   if(input.save)
-  #     :ok
-  #   else
-  #     :bad_request
-  #   end
-  # end
-  #
-  # def login
-  #   user = User.find_by(username: params[:user][:username])
-  #
-  #   if user && user.authenticate(params[:user][:password])
-  #     jwt = Auth.encrypt({ user_id: user.id })
-  #     render json: { jwt: jwt, current: user }
-  #   else
-  #     render json: { error: 'Failed to Log In' }, status: 400
-  #   end
-  # end
-  #
-  # def show
-  #   render json: get_current_user
-  # end
-  #
   private
 
   def user_params
