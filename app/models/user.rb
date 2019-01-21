b78b26ec4968f2c67af378cb9695481a0e868918
bkhartmire <bkhartmire@gmail.com>
2019-01-20 18:16:08 -0800

    sketch out custom user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,11 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
 
+  def invalidate_token
+  end
+
+  def self.validate_login(username, password)
+  end
+
 
 end
