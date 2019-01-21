1f765c2d85d32686ca5c54690d73a02611b4b9a5
bkhartmire <bkhartmire@gmail.com>
2019-01-20 18:17:01 -0800

    code out methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,14 @@ class User < ApplicationRecord
   has_secure_token :auth_token
 
   def invalidate_token
+    self.update_columns(auth_token: nil)
   end
 
   def self.validate_login(username, password)
+    user = find_by(username: username)
+    if user && user.authenticate(password)
+      user
+    end
   end
 
 
