013c157eca5b3f9bf1507276bd0cb95a40e24336
bkhartmire <bkhartmire@gmail.com>
2019-01-21 08:48:23 -0800

    replace console.log with Auth

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,10 @@ class App extends Component {
       }
     }).then(res => res.json())
     .then(res => {
-      console.log(res)
+      Auth.authenticateToken(res.token)
+      this.setState({
+        auth: Auth.isUserAuthenticated(),
+      })
     }).catch(err => console.log(err))
   }
 
