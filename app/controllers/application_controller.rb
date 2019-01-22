72fec5806b62c2b91897da8752cb53e64ffa3a8e
bkhartmire <bkhartmire@gmail.com>
2019-01-21 20:19:39 -0800

    delete unused jwt code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
-  #protect_from_forgery except: :create
-  #def get_current_user
-  #  jwt_token = request.headers['HTTP_AUTHORIZATION'] # .gsub('Bearer ', '')
-#
-  #  if jwt_token
-  #    user_info = Auth.decode(jwt_token)
-  #    user ||= User.find(user_info['user_id'])
-  #  end
 
-  #  user
-  #end
 end
