1fae445064ec19c008746e98f420da2cedd135a3
bkhartmire <bkhartmire@gmail.com>
2019-01-21 23:08:25 -0800

    organize danceclasslist by day of the week

--- client/src/containers/DanceClassList.js
+++ client/src/containers/DanceClassList.js
@@ -11,10 +11,24 @@ class DanceClassList extends Component {
   }
   render(){
     const {danceClasses} = this.props
+
     return(
-      <div>
+      <div className="danceClassList">
         <h1>Dance Classes</h1>
-        {danceClasses.map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Monday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'MONDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Tuesday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'TUESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Wednesday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'WEDNESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Thursday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'THURSDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Friday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'FRIDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Saturday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'SATURDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
+        <h3>Sunday</h3>
+          {danceClasses.filter(danceClass => danceClass.day === 'SUNDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass}/>)}
       </div>
     )
   }
