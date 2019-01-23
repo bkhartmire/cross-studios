734607f5b12d232db965c5dcd079cc6a831809e9
bkhartmire <bkhartmire@gmail.com>
2019-01-22 21:53:13 -0800

    button toggles between add and remove from schedule

--- client/src/components/DanceClass.js
+++ client/src/components/DanceClass.js
@@ -5,22 +5,33 @@ import Auth from '../modules/Auth'
 class DanceClass extends Component {
   handleClick(e, danceClassId) {
     e.preventDefault()
-    const data = {dance_class_id: danceClassId}
-    fetch('/api/user_dance_classes', {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: {
-        token: Auth.getToken(),
-        'Authorization': `Token ${Auth.getToken()}`,
-        'Content-Type': 'application/json',
-      }
-    }).then(res => res.json())
-    .then(response => console.log('Success:', JSON.stringify(response)))
-    .catch(error => console.error('Error:', error));
+    if (e.target.innerHTML === 'Add Class to Schedule') {
+      const data = {dance_class_id: danceClassId}
+      fetch('/api/user_dance_classes', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          token: Auth.getToken(),
+          'Authorization': `Token ${Auth.getToken()}`,
+          'Content-Type': 'application/json',
+        }
+      }).then(res => res.json())
+      .then(alert("Class successfully added to your schedule!"))
+      .catch(error => console.error('Error:', error))
+      e.target.style.backgroundColor = 'red'
+      e.target.innerHTML = "Remove from Schedule"
+    } else {
+      //put delete request here
+      alert('DELETE')
+      e.target.style.backgroundColor = ''
+      e.target.innerHTML = "Add Class to Schedule"
+    }
+
   }
+
   render() {
     const {danceClass} = this.props
-    //only add instructor link to dance classes with a known instructor
+    //only adds instructor link to dance classes with a known instructor
     let instructor_name
     if (danceClass.instructor.name === "TBA") {
       instructor_name = danceClass.instructor.name
@@ -32,12 +43,14 @@ class DanceClass extends Component {
         }
       }}>{danceClass.instructor.name}</Link>
     }
+    let button
+    button = <button onClick={e => this.handleClick(e, danceClass.id)}>Add Class to Schedule</button>
     return(
       <div className="dance_class_listing">
         <h3>{instructor_name}: {danceClass.name}</h3>
         <h4>{danceClass.start_time}-{danceClass.end_time}</h4>
         <h4>{danceClass.studio.name}</h4>
-        <button onClick={e => this.handleClick(e, danceClass.id)}>Add Class to Schedule</button>
+        {button}
         <br></br>
       </div>
     )
