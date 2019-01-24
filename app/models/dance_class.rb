549bd10968c9679c54302694474972587e610e11
bkhartmire <bkhartmire@gmail.com>
2019-01-23 23:25:12 -0800

    delete unnecessary code in dance_class model

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -4,13 +4,6 @@ class DanceClass < ApplicationRecord
   has_many :user_dance_classes
   has_many :users, through: :user_dance_classes
 
-  #add instance method to make day capital letter, then all lowercase
-
-  def self.sort_by_time
-    sorted_dance_classes = self.sort_by{|dance_class| DateTime.parse(dance_class.end_time).strftime("%H:%M:%S")}
-    return sorted_dance_classes
-  end
-
   def self.sort_chronologically
     monday = self.all.select {|dance_class| dance_class.day == "MONDAY"}
     tuesday = self.all.select {|dance_class| dance_class.day == "TUESDAY"}
@@ -25,9 +18,5 @@ class DanceClass < ApplicationRecord
     end
     sorted_days.flatten
   end
-
-
-
-
 
 end
