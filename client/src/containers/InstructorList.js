618dbbdf814997a77198f7b58d63a9c1693f1658
bkhartmire <bkhartmire@gmail.com>
2019-01-24 16:32:15 -0800

    create instructor componenet

--- client/src/containers/InstructorList.js
+++ client/src/containers/InstructorList.js
@@ -3,9 +3,14 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
 import { fetchInstructors } from '../actions/danceClassActions'
+import Instructor from '../components/Instructor'
 
 class InstructorList extends Component {
+  render() {
+    return(
 
+    )
+  }
 }
 
 export default InstructorList
