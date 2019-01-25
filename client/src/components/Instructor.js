1d0681de43333ed17aff8e37041bcb56071e9f7f
bkhartmire <bkhartmire@gmail.com>
2019-01-24 16:46:57 -0800

    all_instructors index page renders link to each instructor profile

--- client/src/components/Instructor.js
+++ client/src/components/Instructor.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Instructor = ({instructor}) =>
   <div className="instructor">
-    {instructor.name}
+    <a href={`/instructors/${instructor.id}`}><h3>{instructor.name}</h3></a>
   </div>
 
 export default Instructor
