33f6906257f3d23449b2ffe7eaf3fe21561a2827
bkhartmire <bkhartmire@gmail.com>
2019-01-24 19:09:57 -0800

    favorites#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
 
   def create
+    @user = User.find_by_auth_token!(request.headers[:token])
+    user_id = @user.id
+    @favorite = Favorite.find_or_create_by(user_id: user_id, instructor_id: params[:instructor_id])
+    render json: @favorite
   end
 
   def destroy
