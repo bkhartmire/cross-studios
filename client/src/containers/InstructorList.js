db7b76a5981ce04e2db27843e7970fa479b8efb0
bkhartmire <bkhartmire@gmail.com>
2019-01-24 16:34:28 -0800

    create componentDidMount and map state/dispatch to props

--- client/src/containers/InstructorList.js
+++ client/src/containers/InstructorList.js
@@ -6,11 +6,24 @@ import { fetchInstructors } from '../actions/danceClassActions'
 import Instructor from '../components/Instructor'
 
 class InstructorList extends Component {
+  componentDidMount( {
+    this.props.fetchInstructors()
+  })
+
   render() {
     return(
-
+      <h1>Hello</h1>
     )
   }
 }
 
-export default InstructorList
+const mapStateToProps = state => {
+  return {
+    instructors: state.instructors.instructor_data
+  }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchInstructors
+}, dispatch)
+export default connect (mapStateToProps, mapDispatchToProps)(InstructorList)
