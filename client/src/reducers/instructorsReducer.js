541d6660f88e1678a815c18d568e8502c3eee6db
bkhartmire <bkhartmire@gmail.com>
2019-01-24 16:16:09 -0800

    load and fetch instructors in reducer

--- client/src/reducers/instructorsReducer.js
+++ client/src/reducers/instructorsReducer.js
@@ -8,6 +8,10 @@ export default function instructorsReducer(
         return {...state, loading: true}
       case 'FETCH_INSTRUCTOR':
         return {...state, loading: false, instructor_data: action.payload}
+      case 'LOADING_INSTRUCTORS':
+        return {...state, loading: true}
+      case 'FETCH_INSTRUCTORS':
+        return {...state, loading: false, instructor_data: action.payload}
       default:
         return state
     }
