3318e1f8590a20e278893052ab5a83ce50c423d5
bkhartmire <bkhartmire@gmail.com>
2019-01-24 16:13:50 -0800

    fetchInstructors action

--- client/src/actions/instructorsActions.js
+++ client/src/actions/instructorsActions.js
@@ -9,5 +9,11 @@ export const fetchInstructor = (instructor_id) => {
 }
 
 export const fetchInstructors = () => {
-  
+  return (dispatch) => {
+    dispatch({type: 'LOADING_INSTRUCTORS'})
+    return fetch('/api/instructors', {
+      accept: 'application/json',
+    }).then(resp => resp.clone().json())
+    .then(instructors => dispatch({type: 'FETCH_INSTRUCTORS', payload: instructors}))
+  }
 }
