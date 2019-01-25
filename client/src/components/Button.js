b4af838fd4a46edd2ffe1227597cbb7a5d4dc419
bkhartmire <bkhartmire@gmail.com>
2019-01-24 19:21:12 -0800

    undo some of the edited button code so that schedueld classes are gray

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,9 +2,7 @@ import React, {Component} from 'react'
 import { addToUserSchedule, removeFromUserSchedule } from '../actions/danceClassActions'
 
 class Button extends Component {
-  // componentDidMount() {
-  //   debugger
-  // }
+
   handleClick(e, id) {
     e.preventDefault();
     (e.target.innerHTML === 'Add Class to Schedule') ? (addToUserSchedule(e, id)) : (removeFromUserSchedule(e, id));
@@ -14,16 +12,16 @@ class Button extends Component {
     const {danceClass, userDanceClasses} = this.props
     //this conditional determines whether the current user has already added this danceclass to their schedule and defines the add/delete button accordingly
     const userDanceClassMatch = userDanceClasses.find(userDanceClass => userDanceClass.id === danceClass.id)
-    let buttonMessage
+    let button
     if (userDanceClassMatch) {
-      buttonMessage = "Remove from Schedule"
+      button = <button onClick={e => this.handleClick(e, danceClass.id)} style={{backgroundColor: 'gray'}}>Remove from Schedule</button>
     } else {
-      buttonMessage = "Add Class to Schedule"
+      button = <button onClick={e => this.handleClick(e, danceClass.id)} >Add Class to Schedule</button>
     }
 
     return(
       <div className="button">
-        <button onClick={e => this.handleClick(e, danceClass.id)} >{buttonMessage}</button>
+        {button}
       </div>
     )
   }
