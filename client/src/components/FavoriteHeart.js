f24483ca85f26aae961e3b001e452bd4159fb8e5
bkhartmire <bkhartmire@gmail.com>
2019-01-24 21:22:23 -0800

    move code into favoriteheart componenet from instructorprofile

--- client/src/components/FavoriteHeart.js
+++ client/src/components/FavoriteHeart.js
@@ -4,8 +4,16 @@ import { favoriteInstructor } from '../actions/instructorActions'
 class FavoriteHeart extends Component {
   render() {
     const {instructor, userFavorites} = this.props
+
+    const userFavoritesMatch = userFavorites.find(favorite => favorite.instructor_id === instructor.id)
+    let heart
+    if (userFavoritesMatch) {
+      heart = <a className="heart" onClick={e => this.handleUnfavorite(e, instructor.id)} style={{color: 'red'}}>&hearts;</a>
+    } else {
+      heart = <a className="heart" onClick={e => this.handleFavorite(e, instructor.id)} style={{color: '#c2c4c6'}}>&hearts;</a>
+    }
     return(
-      <h4>heart</h4>
+      <h4>{heart}</h4>
     )
   }
 }
