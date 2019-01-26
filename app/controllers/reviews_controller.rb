17c9540b53b936a08fc3589e0d54e7850aa0b05a
bkhartmire <bkhartmire@gmail.com>
2019-01-26 11:28:58 -0800

    fix typos

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def create
     user = User.find_by_auth_token!(request.headers[:token])
     instructor = Instructor.find_by(id: params[:instructor_id])
-    review = Review.create(user_id: user.id, instructor_id = instructor.id, text: params[:review])
+    review = Review.create(user_id: user.id, instructor_id: instructor.id, text: params[:review])
     render json: review
   end
 end
--- client/src/actions/reviewActions.js
+++ client/src/actions/reviewActions.js
@@ -3,7 +3,7 @@ import Auth from '../modules/Auth'
 export const createReview = (review, instructorId) => {
   debugger
   return dispatch => {
-    fetch(`/api/instructos/${instructorId}/reviews`, {
+    fetch(`/api/instructors/${instructorId}/reviews`, {
       method: 'POST',
       body: JSON.stringify({ review: review }),
       headers: {
