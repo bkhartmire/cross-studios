ab6cec3ce8921382a36e2a91479be189027de766
bkhartmire <bkhartmire@gmail.com>
2019-01-26 09:07:47 -0800

    refactor api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #just playing with this so that I can see Nokogiri results
   scope '/api' do
     resources :dance_classes, only: [:index]
     resources :instructors do
