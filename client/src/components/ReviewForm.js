27b606bf255dd607e40b999a6af3fb2f225ed614
bkhartmire <bkhartmire@gmail.com>
2019-01-26 09:27:40 -0800

    reviewform

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -15,5 +15,22 @@ class ReviewForm extends Component {
   handleOnSubmit = e => {
     e.preventDefault()
     //add review action that will fetch post request. pass in this.state.text & this.props.instructor id. Userid will be determined by headers and id determined in controller
+    this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+    this.setState({text: '',})
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleOnSubmit} >
+          <label>New Review:</label>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleOnChange}
+          />
+          <input type="submit"/>
+        </form>
+      </div>
+    )
   }
 }
