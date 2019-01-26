3aae589bf4bc6770954d87952e3246a83682b27f
bkhartmire <bkhartmire@gmail.com>
2019-01-26 09:32:14 -0800

    reviewsCOntainer

---
+++ client/src/containers/ReviewsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReviewForm from '../components/ReviewInput'
+import ReviewsList from '../components/ReviewsList'
+import { connect } from 'react-redux'
+
+class ReviewsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <ReviewForm addReview={this.props.addReview} instructorId={this.props.instructor.id}/>
+        <ReviewsList reviews={this.props.reviews} instructorId={this.props.instructor.id}/>
+      </div>
+    )
+  }
+}
