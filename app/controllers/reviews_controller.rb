c4160bcbbccba8bc140403a6357c3d03ea344d98
bkhartmire <bkhartmire@gmail.com>
2019-02-01 10:03:01 -0800

    fix reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,17 +3,15 @@ class ReviewsController < ApplicationController
   def create
     user = User.find_by_auth_token!(request.headers[:token])
     instructor = Instructor.find_by(id: params[:instructor_id])
-    # review = Review.find_or_create_by(user_id: user.id, instructor_id: instructor.id)
-    # review.text = params[:review]
-    # review.save
-    #   #json: { review: @review, duplicate: dup }
     existing_review = Review.find_by(user_id: user.id, instructor_id: instructor.id)
     if !!existing_review
-      user_already_reviewed = existing_review
+      user_already_reviewed = true
+      existing_review.update(text: params[:review])
+      review = existing_review
     else
       user_already_reviewed = false
+      review = Review.create(user_id: user.id, instructor_id: instructor.id, text: params[:review])
     end
-    review = Review.create(user_id: user.id, instructor_id: instructor.id, text: params[:review])
     render json: {review: review, duplicate: user_already_reviewed}
   end
 
--- client/src/reducers/instructorsReducer.js
+++ client/src/reducers/instructorsReducer.js
@@ -3,7 +3,6 @@ export default function instructorsReducer(
     loading: false,
     instructor_data: {},
     all_instructors: [],
-    reviews: [],
   }, action) {
     switch (action.type) {
       case 'LOADING_INSTRUCTOR':
@@ -16,17 +15,12 @@ export default function instructorsReducer(
         return {...state, loading: false, all_instructors: action.payload}
       case 'CREATE_REVIEW':
         let instructor = { ...state.instructor_data }
-
         if (!!action.payload.duplicate) {
-          let duplicate_review = instructor.reviews.find(review => review.user_id === action.payload.duplicate.user_id)
-          debugger
+          let duplicate_review = instructor.reviews.find(review => review.user_id === action.payload.review.user_id)
           duplicate_review.text = action.payload.review.text
-          debugger
         } else {
           instructor.reviews.push(action.payload.review)
         }
-
-        debugger
         return {...state, instructor_data: instructor }
       default:
         return state
