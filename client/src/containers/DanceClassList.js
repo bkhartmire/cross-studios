4a95ec90ecd4155ae8b1e33902ae09a27bc9fb10
bkhartmire <bkhartmire@gmail.com>
2019-02-01 14:14:47 -0800

    danceclass list now has to fetch user to acces userdanceclasses

--- client/src/containers/DanceClassList.js
+++ client/src/containers/DanceClassList.js
@@ -2,49 +2,51 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
-import { fetchDanceClasses, fetchUserDanceClasses, addToUserSchedule, removeFromUserSchedule  } from '../actions/danceClassActions'
+import { fetchDanceClasses, addToUserSchedule, removeFromUserSchedule  } from '../actions/danceClassActions'
+import { fetchUser } from '../actions/userActions'
 import DanceClass from '../components/DanceClass'
 
 class DanceClassList extends Component {
 
   componentDidMount(){
     this.props.fetchDanceClasses()
-    this.props.fetchUserDanceClasses()
+    //this.props.fetchUserDanceClasses()
+    this.props.fetchUser()
   }
 
   render(){
-    const {danceClasses, userDanceClasses} = this.props
+    const {danceClasses, user} = this.props
 
     return(
       <div className="danceClassList">
         <h1>Dance Classes</h1>
         <div className="column">
           <h2 className="day">Monday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'MONDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'MONDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Tuesday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'TUESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'TUESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Wednesday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'WEDNESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'WEDNESDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Thursday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'THURSDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'THURSDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Friday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'FRIDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'FRIDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Saturday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'SATURDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'SATURDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
         <div className="column">
           <h2 className="day">Sunday</h2>
-            {danceClasses.filter(danceClass => danceClass.day === 'SUNDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={userDanceClasses} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
+            {danceClasses.filter(danceClass => danceClass.day === 'SUNDAY').map((danceClass) => <DanceClass key={danceClass.id} danceClass={danceClass} userDanceClasses={user.dance_classes} addToUserSchedule={this.props.addToUserSchedule} removeFromUserSchedule={this.props.removeFromUserSchedule}/>)}
         </div>
       </div>
     )
@@ -54,7 +56,8 @@ class DanceClassList extends Component {
 const mapStateToProps = state => {
   return {
     danceClasses: state.danceClasses.all,
-    userDanceClasses: state.danceClasses.userDanceClasses
+    // userDanceClasses: state.danceClasses.userDanceClasses
+    user: state.user.current
   }
 }
 
