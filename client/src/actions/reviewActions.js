ce9c6e1526f0b09d2b447307d511b287a0afb27f
bkhartmire <bkhartmire@gmail.com>
2019-02-01 10:08:13 -0800

    going to dispatch delete action

--- client/src/actions/reviewActions.js
+++ client/src/actions/reviewActions.js
@@ -11,29 +11,32 @@ export const createReview = (review, instructorId) => {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       }
-    }).then(res => {
-      console.log(res)
-      return res.json()
-    })
+    }).then(resp => resp.json())
     .then(review => dispatch({
       type: 'CREATE_REVIEW',
       payload: review
     }))
     .catch(err => err)
-    //document.location.reload()
   }
 }
 
 export const deleteReview = (id, instructorId) => {
-  fetch(`/api/instructors/${instructorId}/reviews/${id}`, {
-    method: 'DELETE',
-    headers: {
-      token: Auth.getToken(),
-      'Authorization': `Token ${Auth.getToken()}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    }
-  }).then(resp => resp.json())
-  .catch(err => err)
-  document.location.reload()
+  return dispatch => {
+    fetch(`/api/instructors/${instructorId}/reviews/${id}`, {
+      method: 'DELETE',
+      headers: {
+        token: Auth.getToken(),
+        'Authorization': `Token ${Auth.getToken()}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(resp => resp.json())
+    .then(review => dispatch({
+      type: 'DELETE_REVIEW',
+      payload: review
+    }))
+    .catch(err => err)
+  }
+
+  //document.location.reload()
 }
