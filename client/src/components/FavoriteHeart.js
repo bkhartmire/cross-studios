ed9478e97142027652f2713201e7d9bca8b800a9
bkhartmire <bkhartmire@gmail.com>
2019-02-01 22:32:38 -0800

    try out if refactoring favorite/unfavorite methods in heart componenet works

--- client/src/components/FavoriteHeart.js
+++ client/src/components/FavoriteHeart.js
@@ -1,20 +1,20 @@
 import React, {Component} from 'react'
-import { favoriteInstructor, unfavoriteInstructor } from '../actions/userActions'
+
 
 class FavoriteHeart extends Component {
 
-  handleFavorite(e, instructorId) {
-    e.preventDefault()
-    favoriteInstructor(instructorId)
-  }
-
-  handleUnfavorite(e, favoriteId, instructorId) {
-    e.preventDefault()
-    unfavoriteInstructor(favoriteId, instructorId)
-  }
+  // handleFavorite(e, instructorId) {
+  //   e.preventDefault()
+  //   favoriteInstructor(instructorId)
+  // }
+  //
+  // handleUnfavorite(e, favoriteId, instructorId) {
+  //   e.preventDefault()
+  //   unfavoriteInstructor(favoriteId, instructorId)
+  // }
 
   render() {
-    const {instructor, userFavorites} = this.props
+    const {instructor, userFavorites, favoriteInstructor, unfavoriteInstructor} = this.props
 
     let userFavoritesMatch
     if (userFavorites && userFavorites.length > 0) {
@@ -22,9 +22,9 @@ class FavoriteHeart extends Component {
     }
     let heart
     if (!!userFavoritesMatch) {
-      heart = <a className="heart favorited" onClick={e => this.handleUnfavorite(e, userFavoritesMatch.id, instructor.id)}>&hearts;</a>
+      heart = <a className="heart favorited" onClick={unfavoriteInstructor(favoriteId, instructorId)}>&hearts;</a>
     } else {
-      heart = <a className="heart not-favorited" onClick={e => this.handleFavorite(e, instructor.id)} >&hearts;</a>
+      heart = <a className="heart not-favorited" onClick={favoriteInstructor(instructorId))} >&hearts;</a>
     }
     return(
       <span>  {heart}</span>
