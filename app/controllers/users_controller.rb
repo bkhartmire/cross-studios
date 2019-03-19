5ce7f8f2f8117c83044de384101e99843ef87115
bkhartmire <bkhartmire@gmail.com>
2019-03-19 11:20:44 +0900

    comment out a lot of unnecesary token auth code and remove inheritance from apicontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 
-class UsersController < ApiController
-   before_action :require_login, except: [:create]
+class UsersController < ApplicationController
+   # before_action :require_login, except: [:create]
 
    def create
-     user = User.create(user_params)
-     render json: {token: user.auth_token}
+     # user = User.create!(user_params)
+     # render json: {token: user.auth_token}
    end
 
    def profile
-     user = User.find_by_auth_token!(request.headers[:token])
-     render json: user, include: [favorites: {include: :instructor}, dance_classes: {include: [:studio, :instructor]}, reviews: {include: [:instructor]}]
+     # user = User.find_by_auth_token!(request.headers[:token])
+     # render json: user, include: [favorites: {include: :instructor}, dance_classes: {include: [:studio, :instructor]}, reviews: {include: [:instructor]}]
    end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,19 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
-  has_secure_password
-  has_secure_token :auth_token
-
-  def invalidate_token
-    self.update_columns(auth_token: nil)
-  end
-
-  def self.validate_login(username, password)
-    user = find_by(username: username)
-    if user && user.authenticate(password)
-      user
-    end
-  end
+  # has_secure_password
+  # has_secure_token :auth_token
+  #
+  # def invalidate_token
+  #   self.update_columns(auth_token: nil)
+  # end
+  #
+  # def self.validate_login(username, password)
+  #   user = find_by(username: username)
+  #   if user && user.authenticate(password)
+  #     user
+  #   end
+  # end
 
 
 end
