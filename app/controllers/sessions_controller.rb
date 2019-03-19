223651e19704092ca98e8413f4f0a9a9fd5c74ec
bkhartmire <bkhartmire@gmail.com>
2019-03-19 19:03:29 +0900

    sessions#create controller action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,9 +29,19 @@ class SessionsController < ApplicationController
   #   current_user.invalidate_token
   # end
 
-  def create
-  end
+  skip_before_action :authenticate
 
-  def destroy
-  end
+   def create
+     user = User.find_by(email: auth_params[:email])
+     if user.authenticate(auth_params[:password])
+       jwt = Auth.issue({user: user.id})
+       render json: {jwt: jwt}
+     else
+     end
+   end
+
+   private
+     def auth_params
+       params.require(:auth).permit(:email, :password)
+     end
 end
