6230a48e8372832c4f1e1d31f823f2c6b60da935
bkhartmire <bkhartmire@gmail.com>
2019-03-19 19:05:32 +0900

    application controller private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
 
+
+  private
+    def token
+      request.env["HTTP_AUTHORIZATION"].scan(/Bearer
+        (.*)$/).flatten.last
+    end
+    def auth
+      Auth.decode(token)
+    end
+    def auth_present?
+      !!request.env.fetch("HTTP_AUTHORIZATION",
+        "").scan(/Bearer/).flatten.first
+    end
 end
