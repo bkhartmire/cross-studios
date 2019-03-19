295d6f09d5a66fe769d4b06a0972ea3027c480bd
bkhartmire <bkhartmire@gmail.com>
2019-03-19 19:06:13 +0900

    authentication methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate
 
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    if auth_present?
+      user = User.find(auth["user"])
+      if user
+        @current_user ||= user
+      end
+    end
+  end
+  
+  def authenticate
+    render json: {error: "unauthorized"}, status: 401
+      unless logged_in?
+  end
 
   private
     def token
