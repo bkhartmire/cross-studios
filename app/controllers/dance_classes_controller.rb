bc51b2d860fa6aff4b02019a9859436f74b71d5e
bkhartmire <bkhartmire@gmail.com>
2019-03-20 20:56:48 +0900

    scraper is broken...hmm

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,9 +1,9 @@
 class DanceClassesController < ApplicationController
   def index
     #only scrape from millennium website once. check to see if there are already dance classes created from millennium website.
-    if DanceClass.all.none?{|dance_class| dance_class.studio_id == Studio.find_by(name: 'Millennium Dance Complex').id}
-      DanceClassScraper.new.make_dance_classes
-    end
+    # if DanceClass.all.none?{|dance_class| dance_class.studio_id == Studio.find_by(name: 'Millennium Dance Complex').id}
+    #   DanceClassScraper.new.make_dance_classes
+    # end
     classes = DanceClass.sort_chronologically
     #include associated instructor and studio in json object
     render json: classes
