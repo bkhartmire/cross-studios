e342e0c1d0af8350b0d77e1b6ce879a977b243d8
bkhartmire <bkhartmire@gmail.com>
2019-03-20 16:14:03 +0900

    session reducer handles login in success dispatch

--- client/src/reducers/sessionReducer.js
+++ client/src/reducers/sessionReducer.js
@@ -1,5 +1,12 @@
 export default function sessionReducer(
-
-) {
-  
+  state = {
+    session: !!sessionStorage.jwt
+  }, action) {
+    switch (action.type) {
+      case 'LOG_IN_SUCCESS':
+        browserHistory.push('/')
+        return !!sessionStorage.jwt
+      default:
+        return state;
+    }
 }
