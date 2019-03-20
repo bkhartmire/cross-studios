2f36dd06082bdc35a0a7c30ab2785c9e74d11f75
bkhartmire <bkhartmire@gmail.com>
2019-03-20 16:10:04 +0900

    create, import, export session reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,12 +3,14 @@ import danceClassesReducer from './danceClassesReducer'
 import studiosReducer from './studiosReducer'
 import userReducer from './userReducer'
 import instructorsReducer from './instructorsReducer'
+import sessionReducer from './sessionReducer'
 
 const rootReducer = combineReducers({
   danceClasses: danceClassesReducer,
   studios: studiosReducer,
   user: userReducer,
   instructors: instructorsReducer,
+  session: sessionReducer
 })
 
 export default rootReducer
