3c118c363e99a77ae2604f6aea5c83100ab9c340
bkhartmire <bkhartmire@gmail.com>
2019-03-20 22:19:21 +0900

    find user correct way in join table controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,13 @@ class FavoritesController < ApplicationController
 
 
   def index
-    user = User.find_by_auth_token!(request.headers[:token])
+    user = current_user
     favorites = Favorite.all.select{|favorite| favorite.user_id == user.id}
     render json: favorites
   end
 
   def create
-    user = User.find_by_auth_token!(request.headers[:token])
+    user = current_user
     user_id = user.id
     instructor = Instructor.find_by(id: params[:instructor_id])
 
--- app/controllers/user_dance_classes_controller.rb
+++ app/controllers/user_dance_classes_controller.rb
@@ -1,8 +1,7 @@
 class UserDanceClassesController < ApplicationController
 
   def create
-    user = User.find_by_auth_token!(request.headers[:token])
-    user_id = user.id
+    user_id = current_user.id
     userDanceClass = UserDanceClass.create(user_id: user_id, dance_class_id: params[:dance_class_id])
     render json: userDanceClass.dance_class
   end
