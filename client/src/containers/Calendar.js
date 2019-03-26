246cf2217312a540159e9b350cfe3c67530f9d5b
bkhartmire <bkhartmire@gmail.com>
2019-03-26 15:24:46 +0900

    change to weekly view

--- client/src/containers/Calendar.js
+++ client/src/containers/Calendar.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import { DayPilot, DayPilotCalendar } from 'daypilot-pro-react'
 
 class Calendar extends Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      viewType: "Week"
+    }
+  }
   render() {
+    var {...config} = this.state
     return (
       <div>
-        <DayPilotCalendar/>
+        <DayPilotCalendar
+          {...config}
+        />
       </div>
     )
   }
