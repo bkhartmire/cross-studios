5f880f57c578a1497875be1ba6e0c9f771944c56
bkhartmire <bkhartmire@gmail.com>
2019-03-26 15:44:14 +0900

    try out dummy event code in state

--- client/src/containers/Calendar.js
+++ client/src/containers/Calendar.js
@@ -6,7 +6,22 @@ class Calendar extends Component {
     super(props)
     this.state = {
       viewType: "Week",
-      headerDateFormat: "dddd" //name of day (e.g. 'Monday')
+      headerDateFormat: "dddd", //name of day (e.g. 'Monday')
+      events: [
+    {
+      id: 1,
+      text: "Event 1",
+      start: "2019-03-26T10:30:00",
+      end: "2019-03-26T13:00:00"
+    },
+    {
+      id: 2,
+      text: "Event 2",
+      start: "2019-03-26T12:00:00",
+      end: "2019-03-26T14:00:00",
+      backColor: "#38761d"
+    }
+  ]
     }
   }
   render() {
