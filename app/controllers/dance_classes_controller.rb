d3325d3d7dc8af2a2145f0b562aa95cf73bd9ec1
bkhartmire <bkhartmire@gmail.com>
2019-03-27 15:54:55 +0900

    create separate controller action for returning reformatted dance_Classes. going to need both eventually

--- app/controllers/dance_classes_controller.rb
+++ app/controllers/dance_classes_controller.rb
@@ -1,17 +1,19 @@
 class DanceClassesController < ApplicationController
   def index
     #only scrape from millennium website once. check to see if there are already dance classes created from millennium website.
+    classes = DanceClass.sort_chronologically
+    render json: classes
+  end
+
+  def calendar_index
+    #only scrape from millennium website once. check to see if there are already dance classes created from millennium website.
     if DanceClass.all.none?{|dance_class| dance_class.studio_id == Studio.find_by(name: 'Millennium Dance Complex').id}
       DanceClassScraper.new.make_dance_classes
     end
     classes = DanceClass.sort_chronologically
-    #include associated instructor and studio in json object
-    #going to have to reformat dance_class object to fit into calendar component...
     reformatted_classes = classes.map do |dance_class|
       dance_class.reformat
     end
-    binding.pry
-    #render json: classes
     render json: reformatted_classes
   end
 
